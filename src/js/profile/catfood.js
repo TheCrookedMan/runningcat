31a1683afc96a9a72df7f54f4abd4e23e535b722
TheCrookedMan <thecrookedman@163.com>
2016-05-31 18:33:07 +0800

    猫粮

--- src/js/profile/catfood.js
+++ src/js/profile/catfood.js
@@ -1,16 +1,31 @@
 (function() {
-    $.get('/catfood.template', {
-        memberId: 1,
-        isUsed: 0
-    }, function(data) {
-    	$(".pub-list ul").append(data);
-    });
-    
+    list(0);
+    var t=0;
+    function list(isUsed){
+        $.get('/catfood.template', {
+            memberId: 1,
+            isUsed: isUsed
+        }, function(data) {
+            $("#t"+isUsed+" ul").append(data);
+        });
+    }
+    $(".tab-catfood li a").click(function(){
+         var isUsed=$(this).data("id");
+         $(this).addClass('cur').parent().siblings().children().removeClass('cur');
+         if(t==0){
+            list(isUsed);
+            t=1;
+         }
+         $("#t"+isUsed).show().siblings(".pub-list").hide();
+    })
+    $("#sdfsd").unbind("click").click(function(e){
+
+    }); 
     this.scroll.on(bottomCallback,topCallback);
     function bottomCallback(){
-    	debugger
+    	//debugger
     }
     function topCallback(){
-    	debugger
+    	//debugger
     }
 }).call(this);
