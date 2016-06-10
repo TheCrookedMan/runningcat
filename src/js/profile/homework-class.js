03c009321ea33a2ced159e99491cc7cf44dcb074
yueqin.zuo <yueqin.zuo@enjoytouch.com.cn>
2016-06-11 01:14:03 +0800

    作业

--- src/js/profile/homework-class.js
+++ src/js/profile/homework-class.js
@@ -29,8 +29,8 @@
         getHomeWork: function() {
             var self = this;
             $.get('/getCourseWorkInfo.template', {
-                memberId:userInfo.memberId,
-                onceId: onceId,
+                memberId:/*userInfo.memberId*/49,
+                onceId: /*onceId*/55,
                 onceType: 1
             }).success(function(data) {
                 data = data.replace(/(^\s+)|(\s+$)/g, "");
@@ -38,7 +38,7 @@
                     self.isEnd = true;
                 } else {
                     self.isEnd = false;
-                    $(".pub-list ul").append(data);
+                    $(".pub-list ul").prepend(data);
                 }
                // console.log(data)
             }).error(function(err) {});
@@ -47,14 +47,51 @@
     this.homeWork = new homeWork();
     this.homeWork.init();
 
+
     /*查询我的课次作业*/
     $.get('/submitclass.template', {
-        memberId:userInfo.memberId,
-        courseId:onceId,
-        workId:1
+        memberId:/*userInfo.memberId*/46,
+        onceId:/*onceId*/55,
+        workId:11
     }).success(function(data) {
-         //$(".hr").before(data);
+         $(".pub-list ul").append(data);
+         //console.log(data.data)
     }).error(function(err) {});
 
 
+    $(".uploadImage").uploadImage({
+        url: "/common/uploadImage",
+        callback: function(file, data, resp) {
+            var path = data.images[0].userFilePath;
+            $(this).parents(".task").siblings(".task").find(".topicImgUrl").attr('src', window.imageAddress + path);
+            $(this).siblings(".photoUrl").val(path);
+        }
+    });
+
+
+    /*提交作业*/
+    $("body").on("submit",".doCourseWork",function() {
+        var data = common.parseForm("form");
+        $.post('/usr-class/doCourseWork',data).success(function(data){
+            if(data.code == "0000" && data.success){
+                modal.alert("提交作业成功！");
+            } else {
+                modal.alert(data.msg);
+            }
+        })
+        return false;
+    });
+
+    /*修改作业*/
+    $("body").on("submit",".updateCourseWork",function() {
+        var data = common.parseForm("form");
+        $.post('/usr-class/updateCourseWork',data).success(function(data){
+            if(data.code == "0000" && data.success){
+                modal.alert("修改作业成功！");
+            } else {
+                modal.alert(data.msg);
+            }
+        })
+        return false;
+    });
 }).call(this);
